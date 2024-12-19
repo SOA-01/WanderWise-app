84640b224cf0e7d68c4741b70b0de8c4b0cf91d8
Arijus1979 <arijusta@gmail.com>
2024-12-19 17:08:59 +0800

    prod fix

--- app/application/controllers/api_check.rb
+++ app/application/controllers/api_check.rb
@@ -1,25 +1,25 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-module WanderWise
-  # Main class to run the application logic
-  class Main
-    def self.run
-      # ----- 1. Flight API -----
-      flight_mapper = WanderWise::FlightMapper.new(WanderWise::AmadeusAPI.new)
-      params = {
-        originLocationCode: 'TPE',
-        destinationLocationCode: 'LAX',
-        departureDate: (Date.today + 7).strftime('%Y-%m-%d'),
-        adults: 1
-      }
-      flight_mapper.save_flight_info_to_yaml(params, './spec/fixtures/flight-offers-results.yml')
+# module WanderWise
+#   # Main class to run the application logic
+#   class Main
+#     def self.run
+#       # ----- 1. Flight API -----
+#       flight_mapper = WanderWise::FlightMapper.new(WanderWise::AmadeusAPI.new)
+#       params = {
+#         originLocationCode: 'TPE',
+#         destinationLocationCode: 'LAX',
+#         departureDate: (Date.today + 7).strftime('%Y-%m-%d'),
+#         adults: 1
+#       }
+#       flight_mapper.save_flight_info_to_yaml(params, './spec/fixtures/flight-offers-results.yml')
 
-      # ----- 2. NY Times API -----
-      times_mapper = WanderWise::ArticleMapper.new(WanderWise::NYTimesAPI.new)
-      times_mapper.save_articles_to_yaml('Taiwan', './spec/fixtures/nytimes-results.yml')
-    end
-  end
-end
+#       # ----- 2. NY Times API -----
+#       times_mapper = WanderWise::ArticleMapper.new(WanderWise::NYTimesAPI.new)
+#       times_mapper.save_articles_to_yaml('Taiwan', './spec/fixtures/nytimes-results.yml')
+#     end
+#   end
+# end
 
-# To execute the application logic
-WanderWise::Main.run unless File.exist?('./spec/fixtures/flight-offers-results.yml') && File.exist?('./spec/fixtures/nytimes-results.yml')
+# # To execute the application logic
+# WanderWise::Main.run unless File.exist?('./spec/fixtures/flight-offers-results.yml') && File.exist?('./spec/fixtures/nytimes-results.yml')
